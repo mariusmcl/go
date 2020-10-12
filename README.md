48724f7808c5ab341249871bad296ad3665e2ef3
Marius Landsverk <mariuscl@berkeley.edu>
2020-10-12 22:49:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Work - through of the book Deep Learning and the Game of Go by 
+Max Pumperla & Kevin Ferguson
+
+Marius Landserk & Axel Rønold
---
+++ dlgo/gotypes.py
@@ -0,0 +1,22 @@
+import enum
+
+class Player(enum.Enum):  # Lett måte å holde styr på hvilket tall det er som spiller og som er player
+    black = 1
+    white = 2
+
+    @property
+    def other(self):
+        print(self)
+        return Player.black if self == Player.white else Player.white  # Fin logikk for hvem som er neste spiller
+
+
+from collections import namedtuple
+
+class Point(namedtuple('Point', 'row col')):
+    def neighbors(self):
+        return [
+            Point(self.row - 1, self.col),
+            Point(self.row + 1, self.col),
+            Point(self.row, self.col - 1),
+            Point(self.row, self.col + 1)
+        ]
